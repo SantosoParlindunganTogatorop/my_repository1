350f037bcc3063c7ef5030bcdb2ebf692f70d91a
SantosoParlindunganTogatorop <santosotogatorop@gmail.com>
2024-09-20 14:53:18 +0800

    Add files via upload

---
+++ Praktikum-APD/Post-Test/Kelas/Pertemuan_3/Percabangan.py
@@ -0,0 +1,49 @@
+#cuaca = "cerah"
+#if cuaca : "cerah"
+#print("Kamu pergi keluar rumah")
+#print("mahal")
+
+#variabel akan bernilai false jika nilai bernilai kosong, 0,{},(),[],false
+
+#uang = 1
+#if uang: 
+ #   print("Pergi ke pasar!")
+#else:
+ #   print("Pergi ke rumah teman")
+
+
+#nilai = 1
+#if nilai > 0 :
+#    print("Bilangan Positif")
+#else:
+#    print("Bilangan negatif")
+
+
+#nilai = 76
+#if nilai >= 80 :
+#    print("nilai A+")
+#else:
+#    print("nilai A-")
+#    else: 
+#print("tidak lulus")
+
+
+print("Menu")
+print("""1.Nonton film
+2.Ngoding
+3.Keluar aplikasi """)
+
+#keterangan = int(input("Masukkan Menu : "))
+#if keterangan == 1:
+#    print("nonton film")
+
+#elif keterangan == 2: 
+#    print("Ngoding")
+
+#elif keterangan == 3 :
+#    print("Keluar aplikasi")
+
+#else :
+#    print("Tidak ada menu yang tersedia")
+
+
