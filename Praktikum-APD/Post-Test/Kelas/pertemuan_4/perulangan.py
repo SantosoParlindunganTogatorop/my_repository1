b718791d6493e105748d4b107c06b4fa091fcc50
SantosoParlindunganTogatorop <santosotogatorop@gmail.com>
2024-09-27 15:09:00 +0800

    pertemuan 4

---
+++ Praktikum-APD/Post-Test/Kelas/pertemuan_4/perulangan.py
@@ -0,0 +1,62 @@
+
+#perulangan for
+
+# batas = 10
+# for i in range (1,batas,2):
+#     print(f"perulangan ke -{i}")
+
+
+# buah_banyak = ("apel", "mangga", "anggur", "semangka")
+# for buah in buah_banyak :
+#     print(buah)
+
+
+# for baris in range (1,4):
+#     print(f"baris ke {baris}")
+#     for kolom in range (1,4):
+#         print(kolom, "kolom ", end="")
+#     print()
+#     print()
+
+
+# for i in range(1,10):
+#     if i == 4:
+#         break
+#     else:
+#         print(f"perulangan ke-{i}")
+
+
+# for i in range(1,10):
+#     if i % 2== 1:
+#         continue
+#     else:
+#         print(f"perulangan ke-{i}")
+
+
+
+
+#perulangan while
+
+# ulang = 0
+# lagi = "yes"
+# while lagi == "yes":
+#     ulang += 1
+#     lagi = input("mabar lagi gak?")
+# print("selesai")
+# print(f"perulangan terjadi sebanyak {ulang}")
+
+
+# while True:
+#     ulang = input("Mabar lagi ? ")
+#     if ulang == "gak":
+#         break
+# print(f"berhenti mabar")\
+
+for i in range(1) :
+    for j in range (8):
+        print("*")
+        continue
+   
+
+
+
