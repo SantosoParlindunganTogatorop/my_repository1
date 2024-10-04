18b54ce26ce4206a34fc1cbb307f058311a52175
SantosoParlindunganTogatorop <santosotogatorop@gmail.com>
2024-10-04 15:04:35 +0800

    pertemuan 5

---
+++ Praktikum-APD/Post-Test/Kelas/Pertemuan_5/CRUD.py
@@ -0,0 +1,65 @@
+
+# data = [1,2,3,4,True,[2,3],"Adi"]
+# data_2 = list((1,2,3,4,5))
+# print(data[5][0])
+
+#Menambahkan data menggunakan append
+
+# data = ['apel','durian','jeruk']
+# print(data)
+# print()
+
+
+# data.append('semangka')       #berlaku hanya menambah satu data
+# print(data)
+# data.append(True)
+
+# #menambahkan data pada indeks tertentu
+
+# data.insert(0,'Rambutan')         #menambaha "rambutan" ke awal/ indeks petama
+# print(data)
+# print()
+
+# data.extend(["rambutan", "semangka"])       #menambahkan data lebih dari satu
+# print(data)
+# print(len(data))    #untuk menghitung jumlah data
+
+#untuk mengubah nama indeks
+
+# data[0] = "anggur"          #untuk mengganti data apel dengan anggur (tergantung indeks)
+# print(data)
+
+#menghapus list
+
+# del data[2]             #untuk menghapus list indeks 
+                        
+# data.pop (1)          #untuk menghapus list namun masih bisa dicek/ tidak secara permanen
+#data pop yang dihapus disimpan dalam variabel 
+#data_hapus = data.pop(1)
+#print data_hapus
+
+# data.remove("Apel")           #untuk menghapus data tanpa indeks namun namanya harus sesuai
+# print(data)
+
+
+# ]
+
+# print(data_mahasiswa[0][2][0])
+
+# buah = ["apel","jeruk","durian"]
+
+# index = 1
+# for data in buah :
+#     print(f"data ke {index}")
+#     print(data)
+#     index+=1
+#     print("="*10)
+
+# data_mahasiswa= ["ifnu","adi","ucup","michael"]
+# print(data_mahasiswa)
+# data  = int(input("yang ingin diganti : "))
+# data -= 1
+# data1 = input("nama yang ingin diganti :")
+# data_mahasiswa[data]= data1
+# print(data_mahasiswa)
+
